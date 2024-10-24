23d9bc8be68747c66f6c35631cf33ea27dd933a2
Justo Borrás <jborras@mail.austral.edu.ar>
2024-10-24 12:59:38 -0300

    .

--- R/primera_funcion.R
+++ R/primera_funcion.R
@@ -1,11 +1,12 @@
-descarga_datos <- function(id_estacion, ruta_archivo) {
+primera_funcion <- function(id_estacion, ruta_archivo) {
   url_repositorio <- "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/"
 
   estacion_url <- paste0(url_repositorio, id_estacion, ".csv")
 
-  download.file(url = estacion_url, destfile = ruta_archivo)
+  download.file(url = estacion_url, destfile = "dato.csv")
 
-  datos <- read_csv(ruta_archivo)
+  datos <- readr::read_csv(ruta_archivo)
 
   return(datos)
 }
+
