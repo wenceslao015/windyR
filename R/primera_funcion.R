b3b95fccecb3aa8c1e9bdea2f4934c6ff92b71f4
Justo Borrás <jborras@mail.austral.edu.ar>
2024-10-25 11:00:23 -0300

    tratando de exportar las funciones

--- R/primera_funcion.R
+++ R/primera_funcion.R
@@ -1,12 +1,20 @@
-primera_funcion <- function(id_estacion, ruta_archivo) {
-  url_repositorio <- "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/"
+#' Funcion de descarga de datos
+#'
+#' @param id_estacion
+#' @param ruta_archivo
+#'
+#' @return
+#' @export
+#'
+#' @examples
+descarga_datos<- function(id_estacion, ruta_archivo) {
+  url_repositorio <- "https://github.com/wenceslao015/windyR/tree/23d9bc8be68747c66f6c35631cf33ea27dd933a2/datos"
 
   estacion_url <- paste0(url_repositorio, id_estacion, ".csv")
 
-  download.file(url = estacion_url, destfile = "dato.csv")
+  download.file(url = estacion_url, destfile = ruta_archivo)
 
   datos <- readr::read_csv(ruta_archivo)
 
   return(datos)
 }
-
