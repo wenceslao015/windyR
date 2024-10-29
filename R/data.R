eaf13a8649cd70de4b3d83f48e26f87dca5dd451
Justo Borrás <jborras@mail.austral.edu.ar>
2024-10-29 14:31:32 -0300

    Empiezo a comentar el conjunto de datos.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Conjunto de datos: Datos
+#'
+#' descripcion
+#'
+#' @format conjunto de datos con tantas filas y tantas columnas
+#' \describe{
+#'  \item{a}{muestra de datos}
+#'  \item{b}{muestra de datos multiplicada por cinco}
+#' }
+#' @source Repositorio de Github.
+"datos"
