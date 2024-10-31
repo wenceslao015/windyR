0302892480c22bdac24c4f2ddf2e39e1d4a44cbd
wenceslao015 <wentejerina2005@gmail.com>
2024-10-31 10:01:21 -0300

    mejoro viñeta y cambios crear_datos

--- data_raw/creando_datos.R
+++ data_raw/creando_datos.R
@@ -1,10 +1,29 @@
-library(dplyr)
-datos <- bind_rows(
-  estacion_NH0472 %>% group_by(fecha) %>% summarize(horas_frio = mean(horas_frio, na.rm = TRUE)) %>% mutate(estacion = "NH0472"),
-  estacion_NH0910 %>% group_by(fecha) %>% summarize(horas_frio = mean(horas_frio, na.rm = TRUE)) %>% mutate(estacion = "NH0910"),
-  estacion_NH0046 %>% group_by(fecha) %>% summarize(horas_frio = mean(horas_frio, na.rm = TRUE)) %>% mutate(estacion = "NH0046"),
-  estacion_NH0098 %>% group_by(fecha) %>% summarize(horas_frio = mean(horas_frio, na.rm = TRUE)) %>% mutate(estacion = "NH0098"),
-  estacion_NH0437 %>% group_by(fecha) %>% summarize(horas_frio = mean(horas_frio, na.rm = TRUE)) %>% mutate(estacion = "NH0437")
+datos <- dplyr::bind_rows(
+  estacion_NH0472 |>
+    dplyr::group_by(fecha) |>
+    dplyr::summarize(horas_frio = mean(horas_frio, na.rm = TRUE)) |>
+    dplyr::mutate(estacion = "NH0472"),
+
+  estacion_NH0910 |>
+    dplyr::group_by(fecha) |>
+    dplyr::summarize(horas_frio = mean(horas_frio, na.rm = TRUE)) |>
+    dplyr::mutate(estacion = "NH0910"),
+
+  estacion_NH0046 |>
+    dplyr::group_by(fecha) |>
+    dplyr::summarize(horas_frio = mean(horas_frio, na.rm = TRUE)) |>
+    dplyr::mutate(estacion = "NH0046"),
+
+  estacion_NH0098 |>
+    dplyr::group_by(fecha) |>
+    dplyr::summarize(horas_frio = mean(horas_frio, na.rm = TRUE)) |>
+    dplyr::mutate(estacion = "NH0098"),
+
+  estacion_NH0437 |>
+    dplyr::group_by(fecha) |>
+    dplyr::summarize(horas_frio = mean(horas_frio, na.rm = TRUE)) |>
+    dplyr::mutate(estacion = "NH0437")
 )
+
 usethis::use_data(datos)
 
